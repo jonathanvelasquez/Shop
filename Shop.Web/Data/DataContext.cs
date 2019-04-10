17ca9776ce8d09122a72718b7a568d3d2f4c695e
jonathanvelasquez <42391288+jonathanvelasquez@users.noreply.github.com>
2019-04-10 17:47:30 -0500

    BD and Arqui
    creacion de bases de datos y arquitectura

---
+++ Shop.Web/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Shop.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Shop.Web.Entities;
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
